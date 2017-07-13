bd1b77ea8fe8fec2be725de5cfa4d26c6d45f759
Nathan <nathan.wang2@gmail.com>
2017-07-13 13:51:40 -0500

    React ref

--- src/App.js
+++ src/App.js
@@ -1,36 +1,42 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   constructor(){
     super();
-    this.state = {currentEvent: '---'}
-    this.update = this.update.bind(this)
+    this.state = {a: ''}
   }
 
-  update(e){
-    this.setState({currentEvent: e.type})
-  }
+  update(event){ // Value from the input field
+    this.setState({
+      a: this.a.value,
+      b: this.refs.b.value,
+      c: this.c.refs.input.value})
+  }   //findDOMNode(the component).value
+
+  // ref is a way to refrence a node or instance of component in the app
+  // ref returns the node we are referencing
 
   render(){
     return (
       <div>
-        <textarea
-          onKeyPress={this.update}
-          onCopy={this.update}
-          onCut={this.update}
-          onPaste={this.update}
-          onFocus={this.update}
-          onBlur={this.update}
-          onDoubleClick={this.update}
-          onTouchStart={this.update}
-          onTouchMove={this.update}
-          onTouchEnd={this.update}
-          cols="30"
-          rows="10" />
-
-        <h1>{this.state.currentEvent}</h1>
+        <input type="text" ref={node => this.a = node} onChange={this.update.bind(this)} />
+        {this.state.a}
+        <hr />
+        <input type="text" ref="b" onChange={this.update.bind(this)} />
+        {this.state.b}
+        <hr />
+        <Input ref={component => this.c = component} update={this.update.bind(this)} />
+        {this.state.c}
       </div>
     )
+      // MAKE SURE THE INPUT I IS UPPERCASED WHEN REF COMPONENTS!!!
+  }
+}
+
+class Input extends React.Component {
+  render(){
+    return (<input ref="input" type="text" onChange={this.props.update}/>)
   }
 }
 export default App
