833c58ff222fbd0178a474a70d1773c8040dcd52
Nathan <nathan.wang2@gmail.com>
2017-07-19 16:09:48 -0500

    Built a JSX Live Compiler as a React Component

--- src/App.js
+++ src/App.js
@@ -1,55 +1,42 @@
 import React from 'react';
-
-// HOC = Higher Order Component
-const HOC = (InnerComponent) => class extends React.Component {
-  constructor(){
-    super();
-    this.state = {count : 0}
-  }
-
-  update(){
-    this.setState({count : this.state.count + 1})
-  }
-
-  componentWillMount(){
-    console.log('this will mount')
-  }
-
-  render() {
-    return(
-      <InnerComponent update={this.update.bind(this)}
-      {...this.props}
-      {...this.state}
-      />
-    )
-  }
-}
+import './App.css';
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '/* add your jsx here */',
+      output: '',
+      err: ''
+    }
+  }
+
+  update(e){
+    let code = e.target.value
+    try {
+      this.setState({
+        output: window.Babel.transform(code, {presets: ['es2015', 'react']}).code,
+        err: ''
+      })
+    } catch(err) {
+      this.setState({err: err.message})
+    }
+  }
+
   render(){
     return(
       <div>
-        <Button>Button</Button>
-        <hr />
-        <LabelHOC>Label</LabelHOC>
+        <header> {this.state.err} </header>
+        <div className="container">
+          <textarea onChange={this.update.bind(this)} default={this.state.input}
+            />
+            <pre>
+              {this.state.output}
+            </pre>
+        </div>
       </div>
     )
   }
 }
-
-const Button = HOC((props) => <button onClick={props.update}>{props.children} - {props.count}</button>)
-
-class Label extends React.Component {
-  componentWillMount(){
-    console.log("label will mount once")
-  }
-  render() {
-    return (
-      <label onMouseMove={this.props.update}>{this.props.children} - {this.props.count}</label>
-    )
-  }
-}
-
-const LabelHOC = HOC(Label)
 
 export default App
