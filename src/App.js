77c013a723400a26f1681e8421ccd7f06cadbe93
Nathan <nathan.wang2@gmail.com>
2017-07-20 13:00:38 -0500

    Using React.cloneElement to extend functionality of children components

--- src/App.js
+++ src/App.js
@@ -3,35 +3,39 @@ import React from 'react';
 class App extends React.Component {
   render(){
     return(
-      <Parent>
-        <div ClassName="Class A" />
-        <div ClassName="Class B" />
-      </Parent>
+      <Button>
+        <button value="A">A</button>
+        <button value="B">B</button>
+        <button value="C">C</button>
+      </Button>
     )
   }
 }
 
 
 // May want to iterate over children in component
-class Parent extends React.Component {
+class Button extends React.Component {
+  constructor(){
+    super();
+    this.state = {selected: "None"}
+  }
+
+  selectedItem(selected){
+    this.setState({selected})
+  }
+
   render(){
-    // Take in child and return the same child
-    // let items = this.props.children.map(child => child)
-
-    // React has it's own map .map(the children, mapping function)
-    // let items = React.Children.map(this.props.children, child => child)
-
-    // you can also put them into an array
-    let items = React.Children.toArray(this.props.children)
-
-    // You could also use a foreach
-    // let items = React.Children
-    //   .forEach(this.props.children,child => console.log(child.props.ClassName))
-
-    // This will return a single child, if more than one throws err
-    // let items = React.Children.only(this.props.children)
-    console.log(items)
-    return null
+    let func = child =>
+      React.cloneElement(child, {
+        onClick: this.selectedItem.bind(this, child.props.value)})
+// props.children is only a descripter of the children (read only)
+    let items = React.Children.map(this.props.children, func)
+    return (
+      <div>
+        <h2>You have selected {this.state.selected}</h2>
+        {items}
+      </div>
+    )
   }
 }
 
