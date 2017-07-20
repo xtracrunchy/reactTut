e84dce19581f800df8feaee4d7c30ab446481866
Nathan <nathan.wang2@gmail.com>
2017-07-20 12:00:10 -0500

    Iterating through children of component

--- src/App.js
+++ src/App.js
@@ -1,41 +1,37 @@
 import React from 'react';
-import './App.css';
 
 class App extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      input: '/* add your jsx here */',
-      output: '',
-      err: ''
-    }
-  }
-
-  update(e){
-    let code = e.target.value
-    try {
-      this.setState({
-        output: window.Babel.transform(code, {presets: ['es2015', 'react']}).code,
-        err: ''
-      })
-    } catch(err) {
-      this.setState({err: err.message})
-    }
-  }
-
   render(){
     return(
-      <div>
-        <header> {this.state.err} </header>
-        <div className="container">
-          <textarea onChange={this.update.bind(this)} default={this.state.input}
-            />
-            <pre>
-              {this.state.output}
-            </pre>
-        </div>
-      </div>
+      <Parent>
+        <div ClassName="Class A" />
+        <div ClassName="Class B" />
+      </Parent>
     )
+  }
+}
+
+
+// May want to iterate over children in component
+class Parent extends React.Component {
+  render(){
+    // Take in child and return the same child
+    // let items = this.props.children.map(child => child)
+
+    // React has it's own map .map(the children, mapping function)
+    // let items = React.Children.map(this.props.children, child => child)
+
+    // you can also put them into an array
+    let items = React.Children.toArray(this.props.children)
+
+    // You could also use a foreach
+    // let items = React.Children
+    //   .forEach(this.props.children,child => console.log(child.props.ClassName))
+
+    // This will return a single child, if more than one throws err
+    // let items = React.Children.only(this.props.children)
+    console.log(items)
+    return null
   }
 }
 
